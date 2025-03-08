a21f59e6c59325c6715b7a7cc2a353806aa73005
Shuudy <ancieux.jerome059@gmail.com>
2025-03-08 21:57:50 +0100

    feat: add Display component to show input

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+function Display({ input }) {
+    return (
+        <div className="display bg-gray-800 text-white text-right p-4 text-2xl rounded-t">
+            {input}
+        </div>
+    );
+}
+
+export default Display;
